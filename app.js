238da97467b5e1156d0d95a2588c2ec9fb7b630f
sameerunnabi <sameerunnabi5@gmail.com>
2021-12-15 20:54:57 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,43 @@
+// var firstname = prompt("enter your full name")
+// var lastname  = prompt("enter your last name")
+// var fullname  =firstname + lastname
+
+// console.log(fullname)
+
+//  var mobile =prompt("enter your favourite mobile name")
+//  var charsInmobile = mobile.length
+//  console.log(charsInmobile)
+
+// var arr =("pakistani")
+// console.log(arr[7]);
+
+//  var arr = ("Hello World")
+//  console.log(arr[3])
+
+// var arr =("pakistani")
+// console.log(arr)
+
+// var firstname = ("saim ul haq")
+// var lastname  = ("javed")
+// var fullname  =firstname + lastname
+// console.log(fullname)
+
+// var arr = ["hedrabad"]
+// arr[0] =  "ISlamabad";
+// console.log(arr)
+
+
+// var rep = ("and")
+// var result = rep.result("&,and")
+// console.log(result)
+
+var a = "peanut";
+var b = a.toUpperCase();
+alert(b); 
+
+
+
+
+
+
+
